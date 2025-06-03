37bd2efbb955c1351fafa76f8c4693aaad5b1d17
Paolo Biancalana (Mac) <biancalana.paolo@gmail.com>
2025-06-03 19:08:58 +0200

    chore: Remove jest.config.js as part of project restructuring

---
+++ grid-brain-frontend/jest.config.cjs
@@ -0,0 +1,42 @@
+const nextJest = require("next/jest.js");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
+  preset: "ts-jest",
+  moduleNameMapper: {
+    "^@/components/(.*)$": "<rootDir>/src/components/$1",
+    "^@/pages/(.*)$": "<rootDir>/src/pages/$1",
+  },
+  transform: {
+    "^.+.(ts|tsx)$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.jest.json" }],
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/**/*.test.{js,jsx,ts,tsx}",
+    "!src/**/index.{js,jsx,ts,tsx}",
+    "!src/**/types.ts",
+    "!src/**/constants.ts",
+    "!src/pages/_app.tsx",
+    "!src/pages/_document.tsx",
+    "!**/node_modules/**",
+    "!**/vendor/**",
+    "!**/__mocks__/**",
+  ],
+  coverageReporters: ["json", "lcov", "text", "clover", "html"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
+
+module.exports = createJestConfig(customJestConfig);
